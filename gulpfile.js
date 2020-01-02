f113a35dffaf4e75de33ad2ef0d104f5a97500a1
Guilherme Haschel <guilherme.haschel@sulbrasilfidc.com.br>
2020-01-02 15:28:52 -0300

    fix(main-scope): attempt to fix a bug where highlithting strips off
    tried to fix a bug where highlighting is stripped off the html file when angular2inline or
    angular-language-service is installed. Implemented the same highlighting in two scopes, using one as
    fallback/default html
    
    attempt to fix #32

--- gulpfile.js
+++ gulpfile.js
@@ -3,34 +3,44 @@ const merge = require('gulp-merge-json');
 const json5 = require('gulp-json5-to-json');
 const package = require('./package.json');
 const homedir = require('os').homedir().replace('\\', '/');
+const edit = require('gulp-json-editor');
+const scopes = package.contributes.grammars.map(scope => scope.scopeName);
 
 function watch() {
   return gulp.watch('./src/**/*.json5', gulp.series(['compile-json']));
 }
 
-function compile() {
-  return gulp.src('./src/**/*.json5')
-    .pipe(merge({
-      fileName: "angular-html.tmLanguage.json",
-      json5: true
-    }))
-    .pipe(json5({
-      beautify: true,
-    }))
-    .pipe(gulp.dest('./syntaxes'));
+function compile(cb) {
+  scopes.forEach(scope => {
+    gulp.src('./src/**/*.json5')
+      .pipe(merge({
+        fileName: `${scope}.json`,
+        json5: true
+      }))
+      .pipe(json5({
+        beautify: true,
+      }))
+      .pipe(edit({
+        "scopeName": `${scope}`,
+      }))
+      .pipe(gulp.dest('./syntaxes'));
+  });
+
+  cb()
 }
 
-gulp.task('copy', () => {
-  return gulp.src('./syntaxes/angular-html.tmLanguage.json')
+function copy(cb) {
+  scopes.forEach(scope => {
+    gulp.src(`./syntaxes/${scope}.json`)
     .pipe(
-      gulp.dest(homedir + '/.vscode/extensions/ghaschel.vscode-angular-html-' + package.version + '/syntaxes')
+      gulp.dest(`${homedir}/.vscode/extensions/ghaschel.vscode-angular-html-${package.version}/syntaxes`)
     );
-})
+  });
 
-gulp.task('compile', () => {
-  return compile();
-});
-gulp.task('compile-json', gulp.series(['compile', 'copy']));
+  cb();
+}
+
+gulp.task('compile-json', gulp.series([compile, copy]));
 
 gulp.task('watch', () => {
   return watch();

8577655040ebc98fb6cc0a6e079d16e69ee21c30
Guilherme Haschel <ghaschel@gmail.com>
2022-10-14 15:28:29 -0300

    docs(theming): finixhed the docs

--- docs/THEMING.md
+++ docs/THEMING.md
@@ -1,75 +1,196 @@
 # Custom Scope Names
 
-> This file aims to be used as a way to point out the matched scopes with custom matches for themes to be able to support them
+> This file aims to be used as a way to point out the extra-matched scopes for themes to be able to support them
 
 ## Summary
 
 - [Custom Scope Names](#custom-scope-names)
   - [Summary](#summary)
-    - [Doctype](#doctype)
-    - [Comments](#comments)
-    - [Invalid attributes see complete list](#invalid-attributes-see-complete-list)
-    - [Angular directives](#angular-directives)
-    - [String](#string)
-    - [Angular-specific tags](#angular-specific-tags)
-    - [Angular Interpolation](#angular-interpolation)
-    - [HTML Entities](#html-entities)
+- [General](#general)
+  - [Element differentiation (HTML/SVG/Custom/Angular)](#element-differentiation-htmlsvgcustomangular)
+- [Angular](#angular)
+  - [Directives](#directives)
+  - [Interpolations](#interpolations)
+- [HTML](#html)
+  - [Doctype](#doctype)
+  - [Entities](#entities)
+  - [DOM events](#dom-events)
+  - [Generic attributes](#generic-attributes)
+- [SVG](#svg)
+  - [Non-string attributes](#non-string-attributes)
+- [XML](#xml)
+  - [DTD, elements and attributes namespace](#dtd-elements-and-attributes-namespace)
+    - [DTD](#dtd)
+    - [Element Namespace](#element-namespace)
+    - [Attribute Namespace](#attribute-namespace)
+  - [Stylesheet](#stylesheet)
+- [Style tags](#style-tags)
+- [Script tags](#script-tags)
 
-### Doctype
+# General
 
-<span name="doctype"></span>
+## Element differentiation (HTML/SVG/Custom/Angular)
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/doctype.png" title="vscode-angular-html" alt="Doctype" />
+<img src="../assets/tag-differentiation.png" title="Element differentiation" alt="Element differentiation" />
 
-- `<` - (punctuation.definition.tag.begin.html)
-- `!` - (keyword.operator.exclamation)
-- `DOCTYPE` - (entity.name.tag.html.doctype)
-- `html` - (entity.other.attribute-name.html)
-- `"any string"` - ([SEE STRING MATCHING](#string))
-- `>` - (punctuation.definition.tag.end.html)
+- custom-component - `html-template.tag.custom`
+- div - `html-template.tag.html`
+- mat-accordion - `entity.name.tag.html.ng`
+- svg - `html-template.tag.svg`
 
-### Comments
+# Angular
 
-<span name="comments"></span>
+## Directives
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/comments.png" title="vscode-angular-html" alt="Comments" />
+<img src="../assets/angular-directives.gif" title="Angular directives" alt="Angular directives" />
 
-- `<!--` - (punctuation.definition.comment.begin.html)
-- `ANYTHING` - (comment.block)
-- `-->` - (punctuation.definition.comment.end.html)
+- General
+  - `=` - punctuation.separator.key-value.html-template.ng
+- `[binding]`
+  - `[` - punctuation.definition.ng-binding-name.begin.html
+  - `binding` - html-template.ng.attributes.input-binding.first-level
+  - `]` - punctuation.definition.ng-binding-name.end.html
+- `@animationTrigger`
+  - `@` - html-template.ng.attributes.animationtrigger.prefix
+  - `animationTrigger` - html-template.ng.attributes.animationtrigger.variable
+- `(click)`
+  - `(` - punctuation.definition.ng-binding-name.begin.html
+  - `click` - html-template.ng.attributes.event-handler
+  - `)` - punctuation.definition.ng-binding-name.end.html
+- `[(binding)]`
+  - `[(` - punctuation.definition.ng-binding-name.begin.html
+  - `binding` - html-template.ng.attributes.input-binding.first-level
+  - `)]` - punctuation.definition.ng-binding-name.end.html
+- `[(binding)]`
+  - `[(` - punctuation.definition.ng-binding-name.begin.html
+  - `binding` - html-template.ng.attributes.input-binding.first-level
+  - `)]` - punctuation.definition.ng-binding-name.end.html
+- `[style.font-size.px]`
+  - `[` - punctuation.definition.ng-binding-name.begin.html
+  - `style` - html-template.ng.attributes.input-binding.first-level
+  - `font-size` - html-template.ng.attributes.input-binding.second-level
+  - `px` - html-template.ng.attributes.input-binding.third-level
+  - `]` - punctuation.definition.ng-binding-name.end.html
+- `[@visibility]`
+  - `[` - punctuation.definition.ng-binding-name.begin.html
+  - `@` - html-template.ng.attributes.input-binding.animationTrigger.prefix
+  - `visibility` - html-template.ng.attributes.input-binding.first-level
+  - `]` - punctuation.definition.ng-binding-name.end.html
+- `*ngFor`
+  - `*` - html-template.ng.attributes.prefix.sugar
+  - `ngFor` - html-template.ng.attributes.suffix.sugar
+- `ref-assignVariable$`
+  - `ref-` - html-template.ng.attributes.prefix.ref
+  - `assignVariable` - html-template.ng.attributes.suffix.name
+  - `$` - html-template.ng.attributes.suffix.rxjs
+- `let-col$`
+  - `let-` - html-template.ng.attributes.prefix.let
+  - `col` - html-template.ng.attributes.suffix.name
+  - `$` - html-template.ng.attributes.suffix.rxjs
+- `#templateVariable`
+  - `#` - html-template.ng.attributes.template.prefix
+  - `templateVariable` - html-template.ng.attributes.template.variable`
 
-### Invalid attributes [see complete list](DEPRECATED-ATTRIBUTES.md)
+## Interpolations
 
-<span name="invalid-attributes"></span>
+<img src="../assets/angular-interpolations.png" title="Angular Interpolations" alt="Angular Interpolations" />
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/invalid-attributes.png" title="Invalid attributes" alt="Invalid attributes" />
+- `{{` - html-template.ng.interpolation.begin
+- `|`, `||`, `&&` - html-template.ng.expression.operator.logical
+- `.`, `?.`, `!.` - html-template.ng.expression.operator.navigator
+- `+`, `-`, `/`, `*`, `%` - html-template.ng.expression.operator.arithmetic
+- `==`, `===` - html-template.ng.expression.operator.comparison
+- `?`, `:` - html-template.ng.expression.operator.navigator
+- `??`, `&` - html-template.ng.expression.operator.bitwise
+- `}}` - html-template.ng.interpolation.end
 
-- `<` - (punctuation.definition.tag.begin.html) <b>not changed</b>
-- `TAG` - (entity.name.tag.html)
-- `attributes` - (invalid.deprecated.entity.other.attribute-name.html invalid.deprecated.`TAG_NAME`.`TAG_ATTRIBUTE`) ([see DEPRECATED-TAGS list](DEPRECATED-TAGS.md) and [see DEPRECATED-ATTRIBUTES list](DEPRECATED-ATTRIBUTES.md))
-- `=` - (<b>puctuation.separator.key-value.html-template.ng</b> punctuation.separator.key-value.html)
-- `"` - (punctuation.definition.string.begin.html)
-- `STRING` - ([SEE STRING MATCHING](#string))
-- `"` - punctuation.definition.string.end.html
+# HTML
 
-### Angular directives
+## Doctype
 
-<span name="angular-directives"></span>
+<img src="../assets/doctype.png" title="vscode-angular-html" alt="Doctype" />
 
-<img src="https://raw.githubusercontent.com/ghaschel/vscode-angular-html/master/assets/angular-directives.png" title="Angular directives" alt="Angular directives" />
+- `!` - html-template.ng.exclamation.doctype
+- `DOCTYPE` - html-template.ng.doctype
+- `html` - html-template.ng.attribute.doctype
 
-### String
+## Entities
 
-<span name="string"></span>
+<img src="../assets/html-entities.png" title="HTML Entities" alt="HTML Entities" />
 
-### Angular-specific tags
+- `&` - html-template.ng.entities.ampersand
+- `;` - html-template.ng.entities.semicolon
 
-- DO IT
+## DOM events
 
-### Angular Interpolation
+<img src="../assets/dom-events.png" title="DOM events" alt="DOM events" />
 
--- DO IT
+- `onabort` - html-template.ng.attributes.events
 
-### HTML Entities
+## Generic attributes
 
--- DO IT (SEE HTML ENTITIES LIST)
+<img src="../assets/generic-attributes.png" title="Generic attributes" alt="Generic attributes" />
+
+- `class` - html-template.ng.attributes.generic
+
+# SVG
+
+## Non-string attributes
+
+<img src="../assets/svg-processing.png" title="SVG Processing" alt="SVG Processing"/>
+
+- `M`202.35,163.64`l`-4.38-13.1`h`-2.13`v`-3.85`h`9.1`v`3.85`h`-1.82`l`2.05`z`" - `html-template.ng.svg.path.commands`
+
+# XML
+
+## DTD, elements and attributes namespace
+
+<img src="../assets/xml-dtd-namespace.png" title="XML Processing" alt="XML Processing">
+
+### DTD
+
+- `<!ELEMENT DAY (DATE,(HOLIDAY|PROGRAMSLOT+)+)>`
+  - `!` - html-template.ng.exclamation.dtd
+  - `ELEMENT` - html-template.ng.dtd
+  - `DAY` - html-template.ng.attribute.doctype
+  - `HOLIDAY` - html-template.ng.qualifier.dtd
+  - `|`, `+`, `?`, `*` - html-template.ng.occurrence.dtd
+- `<!ELEMENT BANNER (#PCDATA)>`
+  - `#PCDATA` - constant.language.dtd
+
+### Element Namespace
+
+- `<li:book>`
+  - `:` - html-template.ng.tag.colon
+  - `book` - html-template.ng.tag.suffix
+
+### Attribute Namespace
+
+- `xmlns:list="listurl"`
+  - `:` - html-template.ng.attributes.colon
+  - `list` - html-template.ng.attributes.suffix
+
+## Stylesheet
+
+<img src="../assets/xml-stylesheet.png" title="XML Stylesheet" alt="XML Stylesheet">
+
+- ``type="text/xls"`
+  - `xls` - html-template.ng.xmlStylesheet.language.identifier
+
+# Style tags
+
+<img src="../assets/style-tag-css.png" title="Style tag" alt="Style tag" />
+
+- `type="text/css"`
+  - `css` - html-template.ng.style.language.identifier
+- `lang="css"`
+  - `css` - html-template.ng.style.language.identifier
+
+# Script tags
+
+<img src="../assets/script-tag-javascript.png" title="Script tags" alt="Script tag" />
+
+- `type="text/javascript"`
+  - `javascript` - html-template.ng.script.language.identifier
+- `lang="javascript"`
+  - `javascript` - html-template.ng.script.language.identifier
--- src/ts/token-customization/index.ts
+++ src/ts/token-customization/index.ts
@@ -14,7 +14,56 @@ const getTextMateRulesWithoutEmptyOnes = (
   return currentRules.filter(rule => !scopesToBeRemoved.includes(rule.scope));
 };
 
+const updateConfiguration = async (settings: string, value: string, flag: boolean): Promise<void> =>
+  vscode.workspace.getConfiguration('vscode-angular-html').update(settings, value, flag);
+
+const removeLegacyColorCustomizations = async (): Promise<void> => {
+  await updateConfiguration('angularExpression', '', true);
+  await updateConfiguration('htmlDoctypeExclamation', '', true);
+  await updateConfiguration('htmlDoctypeElement', '', true);
+  await updateConfiguration('htmlDoctypeAttributes', '', true);
+  await updateConfiguration('dtdDoctypeExclamation', '', true);
+  await updateConfiguration('dtdDoctypeElement', '', true);
+  await updateConfiguration('dtdDoctypeQuantifier', '', true);
+  await updateConfiguration('dtdDoctypeQualifier', '', true);
+  await updateConfiguration('htmlEntitiesAmpersand', '', true);
+  await updateConfiguration('htmlEntitiesSemicolon', '', true);
+  await updateConfiguration('htmlGenericAttributesFollowedByParameter', '', true);
+  await updateConfiguration('htmlEventsAttributes', '', true);
+  await updateConfiguration('htmlAttributeValueSeparator', '', true);
+  await updateConfiguration('htmlTags', '', true);
+  await updateConfiguration('htmlCustomTags', '', true);
+  await updateConfiguration('angularAndAngularMaterialElementTags', '', true);
+  await updateConfiguration('htmlScriptAttributesLanguageIdentifier', '', true);
+  await updateConfiguration('htmlStyleAttributesLanguageIdentifier', '', true);
+  await updateConfiguration('svgTags', '', true);
+  await updateConfiguration('svgDAttributePathCommands', '', true);
+  await updateConfiguration('angularAnimationTriggerPrefix', '', true);
+  await updateConfiguration('angularAnimationTriggerVariableName', '', true);
+  await updateConfiguration('angularEventHandlerName', '', true);
+  await updateConfiguration('angularBindingAttributeDelimiter', '', true);
+  await updateConfiguration('angularOneWayBindingAnimationTriggerDecorator', '', true);
+  await updateConfiguration('angularOneWayBindingFirstLevelDepth', '', true);
+  await updateConfiguration('angularOneWayBindingSecondLevelDepth', '', true);
+  await updateConfiguration('angularOneWayBindingThirdLevelDepth', '', true);
+  await updateConfiguration('angularPrefixedAttributesLetPrefix', '', true);
+  await updateConfiguration('angularPrefixedAttributesRefPrefix', '', true);
+  await updateConfiguration('angularPrefixedAttributesVariableName', '', true);
+  await updateConfiguration('angularPrefixedAttributesRxjsSuffix', '', true);
+  await updateConfiguration('angularSyntaxSugarAttributesPrefix', '', true);
+  await updateConfiguration('angularSyntaxSugarAttributesName', '', true);
+  await updateConfiguration('angularTemplateVariablePrefix', '', true);
+  await updateConfiguration('angularTemplateVariableName', '', true);
+  await updateConfiguration('angularExpressionOperatorsAndNavigatorsColor', '', true);
+  await updateConfiguration('xmlStylesheetAttributesLanguageIdentifier', '', true);
+  await updateConfiguration('xmlTagNamespaceDivider', '', true);
+  await updateConfiguration('xmlTagNamespaceSuffix', '', true);
+  await updateConfiguration('xmlAttributeNamespaceDivider', '', true);
+  await updateConfiguration('xmlAttributeNamespaceSuffix', '', true);
+};
+
 const disableTokenCustomization = async (): Promise<void> => {
+  await removeLegacyColorCustomizations();
   const config = vscode.workspace.getConfiguration();
 
   const tokenColorCustomizations = config.get('editor.tokenColorCustomizations') as {
@@ -29,6 +78,8 @@ const disableTokenCustomization = async (): Promise<void> => {
     { textMateRules: [...currentRules] },
     vscode.ConfigurationTarget.Global,
   );
+
+  await config.update('vscode-angular-html.colorCustomizations', false, vscode.ConfigurationTarget.Global);
 };
 
 const getActiveRulesFromSettings = (configs: vscode.WorkspaceConfiguration): TextMateRule[] => {
@@ -73,51 +124,63 @@ const updateTokenCustomization = async (): Promise<void> => {
   );
 };
 
+const enableColorCustomization = async (): Promise<void> => {
+  await vscode.workspace.getConfiguration('vscode-angular-html').update('colorCustomizations', true, true);
+};
+
 // prettier-ignore
 const addLegacyColorCustomizations = async (): Promise<void> => {
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularExpression', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlDoctypeExclamation', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlDoctypeElement', '#D7BA7D', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlDoctypeAttributes', '#4EC9B0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('dtdDoctypeExclamation', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('dtdDoctypeElement', '#D7BA7D', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('dtdDoctypeQuantifier', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('dtdDoctypeQualifier', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlEntitiesAmpersand', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlEntitiesSemicolon', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlGenericAttributesFollowedByParameter', '#4EC9B0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlEventsAttributes', '#4EC9B0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlAttributeValueSeparator', '#D7BA7D', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlTags', '#569CD6', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlCustomTags', '#569CD6', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularAndAngularMaterialElementTags', '#D7BA7D', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlScriptAttributesLanguageIdentifier', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('htmlStyleAttributesLanguageIdentifier', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('svgTags', '#569CD6', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('svgDAttributePathCommands', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularAnimationTriggerPrefix', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularAnimationTriggerVariableName', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularEventHandlerName', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularBindingAttributeDelimiter', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularOneWayBindingAnimationTriggerDecorator', '#D7BA7D', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularOneWayBindingFirstLevelDepth', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularOneWayBindingSecondLevelDepth', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularOneWayBindingThirdLevelDepth', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularPrefixedAttributesLetPrefix', '#569CD6', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularPrefixedAttributesRefPrefix', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularPrefixedAttributesVariableName', '#4EC9B0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularPrefixedAttributesRxjsSuffix', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularSyntaxSugarAttributesPrefix', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularSyntaxSugarAttributesName', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularTemplateVariablePrefix', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularTemplateVariableName', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('angularExpressionOperatorsAndNavigatorsColor', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('xmlStylesheetAttributesLanguageIdentifier', '#DCDCAA', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('xmlTagNamespaceDivider', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('xmlTagNamespaceSuffix', '#C586C0', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('xmlAttributeNamespaceDivider', '#D16969', true);
-  await vscode.workspace.getConfiguration('vscode-angular-html').update('xmlAttributeNamespaceSuffix', '#C586C0', true);
+  await updateConfiguration('angularExpression', '#D16969', true);
+  await updateConfiguration('htmlDoctypeExclamation', '#C586C0', true);
+  await updateConfiguration('htmlDoctypeElement', '#D7BA7D', true);
+  await updateConfiguration('htmlDoctypeAttributes', '#4EC9B0', true);
+  await updateConfiguration('dtdDoctypeExclamation', '#C586C0', true);
+  await updateConfiguration('dtdDoctypeElement', '#D7BA7D', true);
+  await updateConfiguration('dtdDoctypeQuantifier', '#D16969', true);
+  await updateConfiguration('dtdDoctypeQualifier', '#C586C0', true);
+  await updateConfiguration('htmlEntitiesAmpersand', '#C586C0', true);
+  await updateConfiguration('htmlEntitiesSemicolon', '#C586C0', true);
+  await updateConfiguration('htmlGenericAttributesFollowedByParameter', '#4EC9B0', true);
+  await updateConfiguration('htmlEventsAttributes', '#4EC9B0', true);
+  await updateConfiguration('htmlAttributeValueSeparator', '#D7BA7D', true);
+  await updateConfiguration('htmlTags', '#569CD6', true);
+  await updateConfiguration('htmlCustomTags', '#569CD6', true);
+  await updateConfiguration('angularAndAngularMaterialElementTags', '#D7BA7D', true);
+  await updateConfiguration('htmlScriptAttributesLanguageIdentifier', '#DCDCAA', true);
+  await updateConfiguration('htmlStyleAttributesLanguageIdentifier', '#DCDCAA', true);
+  await updateConfiguration('svgTags', '#569CD6', true);
+  await updateConfiguration('svgDAttributePathCommands', '#C586C0', true);
+  await updateConfiguration('angularAnimationTriggerPrefix', '#D16969', true);
+  await updateConfiguration('angularAnimationTriggerVariableName', '#DCDCAA', true);
+  await updateConfiguration('angularEventHandlerName', '#DCDCAA', true);
+  await updateConfiguration('angularBindingAttributeDelimiter', '#D16969', true);
+  await updateConfiguration('angularOneWayBindingAnimationTriggerDecorator', '#D7BA7D', true);
+  await updateConfiguration('angularOneWayBindingFirstLevelDepth', '#DCDCAA', true);
+  await updateConfiguration('angularOneWayBindingSecondLevelDepth', '#C586C0', true);
+  await updateConfiguration('angularOneWayBindingThirdLevelDepth', '#DCDCAA', true);
+  await updateConfiguration('angularPrefixedAttributesLetPrefix', '#569CD6', true);
+  await updateConfiguration('angularPrefixedAttributesRefPrefix', '#D16969', true);
+  await updateConfiguration('angularPrefixedAttributesVariableName', '#4EC9B0', true);
+  await updateConfiguration('angularPrefixedAttributesRxjsSuffix', '#C586C0', true);
+  await updateConfiguration('angularSyntaxSugarAttributesPrefix', '#D16969', true);
+  await updateConfiguration('angularSyntaxSugarAttributesName', '#DCDCAA', true);
+  await updateConfiguration('angularTemplateVariablePrefix', '#D16969', true);
+  await updateConfiguration('angularTemplateVariableName', '#DCDCAA', true);
+  await updateConfiguration('angularExpressionOperatorsAndNavigatorsColor', '#C586C0', true);
+  await updateConfiguration('xmlStylesheetAttributesLanguageIdentifier', '#DCDCAA', true);
+  await updateConfiguration('xmlTagNamespaceDivider', '#D16969', true);
+  await updateConfiguration('xmlTagNamespaceSuffix', '#C586C0', true);
+  await updateConfiguration('xmlAttributeNamespaceDivider', '#D16969', true);
+  await updateConfiguration('xmlAttributeNamespaceSuffix', '#C586C0', true);
+  await enableColorCustomization();
   await updateTokenCustomization();
 };
 
-export { updateTokenCustomization, addLegacyColorCustomizations, disableTokenCustomization };
+export {
+  updateTokenCustomization,
+  addLegacyColorCustomizations,
+  disableTokenCustomization,
+  updateConfiguration,
+  enableColorCustomization,
+  removeLegacyColorCustomizations,
+};
